03b5eddeaaeea0ab1b45448ac7356026fb70bb33
Jacob Vorreuter <jake@heroku.com>
2010-11-16 16:37:01 -0800

    read from cache on load

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require.paths.unshift(__dirname + '/vendor/');
 
 var fs = require('fs'),
     sys = require('sys'),
+    http = require('http'),
     ws = require('node-websocket-server/lib/ws'),
     server = ws.createServer(),
     redis = require('redis-helper'),
@@ -11,8 +12,25 @@ var fs = require('fs'),
 var conns = new Array();
 var channels = new Array();
 var cache = new Array();
+var cache_length = 300;
 
 Mu.templateRoot = './public';
+
+/*var headers = {'Authorization': 'Basic ' + base64.encode(new Buffer('tempo:' + process.env['TEMPO_PASSWORD']))};
+var client = http.createClient(80, process.env['ION_HOST']);
+var request = client.request('GET', '/instances/slot_names?cloud=' + process.env['CLOUD'] + '&slot=hermes', headers);
+request.end();
+request.on('response', function (response) {
+  response.on('data', function (data) {
+    var channel_list = eval(data.toString('utf8'));
+    if (channel_list) {
+      for(var i=0; i<channel_list.length; i++) {
+        channels.push(channel_list[i]);
+        exports.setup_channel(channel_list[i]);
+      }
+    }
+  });
+});*/
 
 var config = fs.createReadStream('priv/config.js', {'encoding':'UTF-8'});
 config.addListener('error', function(err) {
@@ -25,7 +43,7 @@ config.addListener('data', function(data) {
   var channel_list = eval(data.toString('utf8'));
   if (channel_list) {
     for(var i=0; i<channel_list.length; i++) {
-      channels.push({"name": channel_list[i], "label": channel_list[i].replace('stats.', '')});
+      channels.push(channel_list[i]);
       exports.setup_channel(channel_list[i]);
     }
   }
@@ -41,10 +59,10 @@ exports.setup_channel = function(channel) {
 };
 
 exports.route_msg = function(channel, msg) {
+  if (cache[channel].length > cache_length) cache[channel].shift();
+  cache[channel].push(JSON.parse(msg));
   for (var conn in conns[channel]) {
     console.log('sending ' + msg + ' to ' + conns[channel][conn].id);
-    if (cache[channel].length > 100) cache[channel].shift();
-    cache[channel].push(msg);
     conns[channel][conn].write(msg);
   }
 };
@@ -80,11 +98,14 @@ server.listen(8080);
 require('http').createServer(function (request, response) {
   console.log(request.url);
   if (request.url == '/') {
+    var datapoints = new Array();
+    for (var channel in channels) {
+      datapoints.push({"name": channels[channel], "label": channels[channel].replace('stats.', ''), "data": cache[channels[channel]]});
+    }
     var view = {
       ws_host: process.env['LOCAL_IP'] || 'localhost',
       ws_port: 8080,
-      instances: channels,
-      cache: cache
+      channels: datapoints
     };
     Mu.render('index.html', view, {}, function (err, output) {
       if (err) {
